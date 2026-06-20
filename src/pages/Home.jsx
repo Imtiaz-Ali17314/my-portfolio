5f645af2fa5e41f7bb6153188c9dbb1f5dec3873
Imtiaz Ali <imtiazali17314@gmail.com>
2026-06-20 14:54:21 +0500

    feat: create Home page component with integrated section modules

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,8 +12,8 @@ const Home = () => {
     <>
       <Hero />
       <About />
-      <Experience />
       <Skills />
+      <Experience />
       <Projects />
       <Journey />
       <Achievements />
